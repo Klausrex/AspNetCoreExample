63fcb00052af8ab8e19f4d83bbe45514e37180ea
Klaus Sprafke <klaus.sprafke@siemens.com>
2020-01-22 10:44:30 +0100

    Fixed errors

---
+++ Values.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    static class Globals
+    {
+        static volatile public List<int> values = new List<int>() { 0 };
+    }
+}
