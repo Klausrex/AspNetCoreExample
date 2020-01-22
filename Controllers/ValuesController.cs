cabc004fa32f128ecf630ddf667a89f13c30967b
Klaus Sprafke <klaus.sprafke@siemens.com>
2020-01-22 09:26:32 +0100

    Initial commit

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private List<int> values;
+
+        ValuesController()
+        {
+            values = new List<int>();
+        }
+
+        // GET api/GetLastAndAddNextToList
+        [HttpGet]
+        public int GetLastAndAddNextToList()
+        {
+            var last = values.Last();
+            values.Add(last++);
+            return last;
+        }
+    }
+}
